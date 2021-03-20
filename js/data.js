deba9f5b5d236a3bc75223ae26c3ea5c22a05444
Michal Bartosik <msbartosik@gmail.com>
2021-03-20 19:13:08 +0100

    set up project

---
+++ js/data.js
@@ -0,0 +1,20 @@
+export const data = [
+  {
+      name: 'Tanya Sinclair',
+      job: 'UX Engineer',
+      text: `“I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.”`,
+      image: './images/image-tanya.jpg',
+  },
+  {
+      name: 'John Tarkpor',
+      job: 'Junior Front-end Developer',
+      text: `“If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.”`,
+      image: './images/image-john.jpg',
+  },
+  {
+      name: 'Kate Williams',
+      job: 'Junior JavaScript Developer',
+      text: `“I would recommend this course to anyone who didn't have any prior coding experience. The skills I learned during the course helped me to find my first job as a junior JavaScript developer”`,
+      image: './images/image-kate.jpg',
+  }
+]
---
+++ js/index.js
@@ -0,0 +1,38 @@
+import { data } from './data.js'
+
+const testimonial = document.querySelector('.js-testimonial')
+const name = document.querySelector('.js-name')
+const image = document.querySelector('.js-image')
+const job = document.querySelector('.js-job')
+const quote = document.querySelector('.js-text')
+const nextBtn = document.querySelector('.js-btn-next')
+const prevBtn = document.querySelector('.js-btn-prev')
+
+let index = 0;
+
+const updateSlide = () => {
+  image.classList.add('fade-to-right');
+  image.addEventListener('transitionend', () => {
+    image.setAttribute('src', data[index].image)
+    image.setAttribute('alt', data[index].name)
+    image.classList.remove('fade-to-right');
+  })
+
+  testimonial.classList.add('fade-to-left');
+  testimonial.addEventListener('transitionend', () => {
+    name.innerText = data[index].name
+    job.innerText = data[index].job
+    quote.innerText = data[index].text
+    testimonial.classList.remove('fade-to-left');
+  })
+}
+
+nextBtn.addEventListener('click', () => {
+  index = (index + 1) % data.length
+  updateSlide()
+})
+
+prevBtn.addEventListener('click', () => {
+  index = (index - 1 + data.length) % data.length
+  updateSlide()
+})
